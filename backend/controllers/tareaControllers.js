b48f564863580e82b2dbfb87de501ff98d49c5f5
Juan Carlos Villacorta <jcvillacorta@gmail.com>
2022-03-10 22:23:50 -0500

    CRUD configurado con MongoDB

--- backend/controllers/tareaControllers.js
+++ backend/controllers/tareaControllers.js
@@ -1,8 +1,13 @@
 // Como con Mongoose se trabaja con promesas, se trabaja con async y await
 const asyncHandler = require('express-async-handler');
 
+const Tarea = require('../models/tareaModel')
+
 const getTareas = asyncHandler (async(req, res) => {
-    res.status(200).json({message: 'Obtener Tareas'})
+
+    const tareas = await Tarea.find()
+
+    res.status(200).json(tareas)
 })
 
 const postTareas = asyncHandler (async(req, res) => {
@@ -12,15 +17,42 @@ const postTareas = asyncHandler (async(req, res) => {
        throw new Error('Por favor, teclea una tarea')
     }
 //    console.log(req.body)
-    res.status(200).json({message: 'Crear una Tarea'})
+
+    const tarea = await Tarea.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(tarea)
 })
 
 const putTareas = asyncHandler (async(req, res) => {
-    res.status(200).json({message: `Modificar Tarea ${req.params.id}`})
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if(!tarea){
+        res.status(400)
+        throw new Error ('Tarea no encontrada')
+    }
+
+    const tareaUpdated = await Tarea.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(tareaUpdated)
 })
 
 const delTareas = asyncHandler (async(req, res) => {
-    res.status(200).json({message: `Eliminar Tarea ${req.params.id}`})
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if(!tarea){
+        res.status(400)
+        throw new Error ('Tarea no encontrada')
+    }
+
+    await tarea.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
