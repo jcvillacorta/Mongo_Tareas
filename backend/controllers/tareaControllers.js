20496c050850fc9be62f0dcdc56a3e341b883bc8
Juan Carlos Villacorta <jcvillacorta@gmail.com>
2022-03-10 21:11:11 -0500

    Antes de conectar a MongoDB

--- backend/controllers/tareaControllers.js
+++ backend/controllers/tareaControllers.js
@@ -1,18 +1,27 @@
-const getTareas = (req, res) => {
+// Como con Mongoose se trabaja con promesas, se trabaja con async y await
+const asyncHandler = require('express-async-handler');
+
+const getTareas = asyncHandler (async(req, res) => {
     res.status(200).json({message: 'Obtener Tareas'})
-}
+})
 
-const postTareas = (req, res) => {
+const postTareas = asyncHandler (async(req, res) => {
+    if(!req.body.text) {
+       // res.status(400).json({message: 'Por favor, teclea una tarea'})
+       res.status(400)
+       throw new Error('Por favor, teclea una tarea')
+    }
+//    console.log(req.body)
     res.status(200).json({message: 'Crear una Tarea'})
-}
+})
 
-const putTareas = (req, res) => {
+const putTareas = asyncHandler (async(req, res) => {
     res.status(200).json({message: `Modificar Tarea ${req.params.id}`})
-}
+})
 
-const delTareas = (req, res) => {
+const delTareas = asyncHandler (async(req, res) => {
     res.status(200).json({message: `Eliminar Tarea ${req.params.id}`})
-}
+})
 
 module.exports = {
     getTareas,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT
 
 const app = express()
 
-app.use('/api/tareas', require('./routes/tareaRoutes'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
+app.use('/api/tareas', require('./routes/tareaRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`El servidor inició en el puerto ${port}`))
